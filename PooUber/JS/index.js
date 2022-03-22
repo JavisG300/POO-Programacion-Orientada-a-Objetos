6a25708b604339c609db87499aff0d2c82494738
Javier Andrade <javieregonzaleza@gmail.com>
2022-03-22 14:09:07 -0600

    Herencia en JavaScript

--- PooUber/JS/index.js
+++ PooUber/JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("K98AZW", new Account("Javier González", "INE JGA 123"))
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("AW456", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
