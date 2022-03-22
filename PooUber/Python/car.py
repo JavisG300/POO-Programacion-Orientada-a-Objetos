e622b522833cf59c87c1e92e8f39a6c215e144ea
Javier Andrade <javieregonzaleza@gmail.com>
2022-03-22 12:55:55 -0600

    Método constructor en Python

--- PooUber/Python/car.py
+++ PooUber/Python/car.py
@@ -1,5 +1,11 @@
+from account import Account #Como se cambia el tipo de dato en driver debemos importar la clase
+
 class Car:
     id        = int
     license   = str
-    driver    = str
+    driver    = Account("","") #Cambiamos el tipo de dato a Account
     passenger = int
+
+    def __init__(self, license, driver):
+        self.license    = license
+        self.driver     = driver
--- PooUber/Python/main.py
+++ PooUber/Python/main.py
@@ -1,13 +1,13 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__": #El metodo de condicion de enrada de la aplicacion
     print("Hola mundo")
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
-    print(vars(car))
 
-    car2 = Car()
-    car2.license = "K98AZW"
-    car2.driver = "Javier Gonzalez"
+    car = Car("AMS234", Account("Andres Herrera", "INE ANDA876"))
+    print(vars(car))
+    print(vars(car.driver))
+
+    car2 = Car("K98AZW", Account("Javier Gonzalez", "INE javs"))
     print(vars(car2))
+    print(vars(car2.driver))
